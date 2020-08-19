da46219ced010bd972ae26a378b6b7631aa27255
FuRinYa1030 <sf238238@gmail.com>
2020-08-19 22:57:42 +0900

    2020-08-19-22:57

--- JavaScript/cosmos_data/config.js
+++ JavaScript/cosmos_data/config.js
@@ -1,8 +1,8 @@
 const config = {
-  endpoint: "https://furinya1030.documents.azure.com:443/",
-  key: "kozbiKpDOuo6lKkyifsc1q0qgzrSElXDeyOqb98Q5gibWwibt5j8jjrIyB7OxZK1VfcR0RL8YaRGuSVzJdZu0Q==",
-  databaseId: "SUSHIPASSPORT_Database",
-  containerId: "PersonalDataList",
+  endpoint: "https://2020-2021-sotuken-cosmosdb.documents.azure.com:443/",
+  key: "MtIcblzFIYOMnbz6mdeQQEVIUAt3xUf9t4HqR4A0eIIqVLPnFB8TyeNhcLbebw7d6cx20Kz23aMYbJnD6YgX4A==",
+  databaseId: "2020-2021-sotuken-CosmosDB-Container",
+  containerId: "DataList",
   partitionKey: { kind: "Hash", paths: ["/category"] }
 };
 
