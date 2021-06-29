c4adcab5ccb15de740bd0a63975d7e723351aa1d
schoolPC-SF <2016b15@wakayama.kosen-ac.jp>
2021-06-29 16:38:41 +0900

    2021/02/15

--- JavaScript/cosmos_data/config.js
+++ JavaScript/cosmos_data/config.js
@@ -1,6 +1,6 @@
 const config = {
   endpoint: "https://2020-2021-sotuken-cosmosdb.documents.azure.com:443/",
-  key: "MtIcblzFIYOMnbz6mdeQQEVIUAt3xUf9t4HqR4A0eIIqVLPnFB8TyeNhcLbebw7d6cx20Kz23aMYbJnD6YgX4A==",
+  key: "",
   databaseId: "2020-2021-sotuken-CosmosDB-Container",
   containerId: "DataList",
   partitionKey: { kind: "Hash", paths: ["/category"] }
